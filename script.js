3fa5c2ba1aeff1f205cf542b7e7cbcf6fc8e4c72
Christian Faatoafe <chris_sila39@hotmail.com>
2020-09-23 00:45:28 -0800

    Couldn't figure out the rest so here's what I have

--- script.js
+++ script.js
@@ -11,18 +11,18 @@
 //
 // * Gather user input with prompt's and confirm's
 
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var numChar = "0123456789";
-var specialChar = "!\"#$%&'()*+,-./:;<=>?@[]^_{}~";
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChar = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
+var pwLength = "";
 
 function generatePassword() {
 
-  document.querySelector("#password").textContent = "";
-
-  var length = prompt("Enter the length you wish for your password. (Between 8 - 128 characters)");
-  if (length < 8 || length > 128 || length === " ") {
+  var pwLength = prompt("Enter the length you wish for your password. (Between 8 - 128 characters)");
+  while (pwLength < 8 || pwLength > 128 || pwLength === " ") {
     alert("Please choose a password length between 8 and 128.");
+    var pwLength = prompt("Enter the length you wish for your password. (Between 8 - 128 characters)");
   }
 
   var uppercase = confirm("Would you like to include uppercase letters?");
@@ -34,33 +34,30 @@ function generatePassword() {
     alert("Please select at least one criteria.");
   }
 
-  var pool = [];
+  var pwChar = [];
 
-  if (uppercase) pool.push("uppercase");
-  if (lowercase) pool.push("lowercase");
-  if (numbers) pool.push("numbers");
-  if (special) pool.push("special");
+  if (uppercase) {
+    pwChar = pwChar.concat(upperChar);
+  }
+
+  if (lowercase) {
+    pwChar = pwChar.concat(lowerChar);
+  }
+
+  if (numbers) {
+    pwChar = pwChar.concat(numChar);
+  }
+
+  if (special) {
+    pwChar = pwChar.concat(specialChar);
+  }
 
   var password = "";
 
-  for (var i = 0; i <= length; i++) {
-    var groupIndex = Math.floor(Math.random * pool.length)
-    var group = null;
+  for (var i = 0; i < pwLength; i++) {
+    password = password + pwChar[Math.floor(Math.random() * pwChar.length)];
 
-    if (pool[groupIndex] === "uppercase") {
-      group = uppercase;
-    }
-    if (pool[groupIndex] === "lowercase") {
-      group = lowercase;
-    }
-    if (pool[groupIndex] === "numbers") {
-      group = numbers;
-    }
-    if (pool[groupIndex] === "special") {
-      group = special;
-    }
-
-
+    return password;
   }
 }
 
